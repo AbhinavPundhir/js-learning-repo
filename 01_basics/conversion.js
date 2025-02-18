141c19d3506fb4f1a7b9669afe0c42ab93066ef0
ABHINAV PUNDHIR <abhinav.pundhir25@gmail.com>
2025-02-18 13:48:54 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,13 @@
+let num = undefined;
+
+console.log(typeof(num))
+const valueinNum = Number(num);
+
+console.log(typeof(valueinNum));
+console.log(valueinNum);
+
+// "33" => 33
+// "33abc" -> NAN
+// "" -> 0
+// null -> 0
+// undefined -> NAN
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version.
+
+//alert("hello")// we are running on nodejs not browser
+
+console.log("Abhinav")
+
+console.log(3+3)
+
+//null -> a standalone value in js. This has a type "object" instead of a special type defined for it.
+
+//undefined -> when we don't mention the value for a particular data type. This has a type "undefined" specially for it.
