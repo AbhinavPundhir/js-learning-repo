2ee2b99b15be3a59d269b7f7bee3e2cf63e2d9c9
ABHINAV PUNDHIR <abhinav.pundhir25@gmail.com>
2025-02-21 19:01:15 +0000

    truthy_falsy_values.js

---
+++ 03_basics/truthy.js
@@ -0,0 +1,8 @@
+const num = []
+
+if(num) console.log("Valid value is found.");
+else console.log("No valid value is found.");
+
+//fallsy values
+// 0, null, undefined, -0, BigInt 0n, "", false, Nan.
+
